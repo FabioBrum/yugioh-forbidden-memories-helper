969c0562b4ebb28a6bce174e69097a6bc7f072d4
FabioBrum <fabiosbrum@hotmail.com>
2023-07-02 11:18:54 -0300

    Inserting check when starting koin

--- app/src/main/java/com/example/yugioh_forbidden_memories_helper/MainActivity.kt
+++ app/src/main/java/com/example/yugioh_forbidden_memories_helper/MainActivity.kt
@@ -2,33 +2,34 @@ package com.example.yugioh_forbidden_memories_helper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.lifecycleScope
 import com.example.database.di.databaseModule
 import com.example.domain.di.domainModule
-import com.example.domain.usecases.InitializeDatabaseIfNeededUseCaseImpl
-import com.example.network.OnlineCardsRepositoryImpl
 import com.example.network.di.networkModule
-import kotlinx.coroutines.launch
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext
 import org.koin.core.context.startKoin
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startKoin {
-            // Log Koin into Android logger
-            androidLogger()
-            // Reference Android context
-            androidContext(this@MainActivity)
-            // Load modules
-            modules(listOf(
-                domainModule,
-                networkModule,
-                databaseModule,
-                mainModule
-            ))
+        if(GlobalContext.getKoinApplicationOrNull() == null) {
+            startKoin {
+                // Log Koin into Android logger
+                androidLogger()
+                // Reference Android context
+                androidContext(this@MainActivity)
+                // Load modules
+                modules(
+                    listOf(
+                        domainModule,
+                        networkModule,
+                        databaseModule,
+                        mainModule
+                    )
+                )
+            }
         }
 
         setContentView(R.layout.activity_main)
