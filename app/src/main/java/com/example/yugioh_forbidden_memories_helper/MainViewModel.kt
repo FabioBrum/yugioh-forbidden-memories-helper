152ed35c5783d4e2444fccb10ed426dc779fca09
FabioBrum <fabiosbrum@hotmail.com>
2023-07-08 18:13:49 -0300

    Uploading app images to assets file

--- app/src/main/java/com/example/yugioh_forbidden_memories_helper/MainViewModel.kt
+++ app/src/main/java/com/example/yugioh_forbidden_memories_helper/MainViewModel.kt
@@ -3,7 +3,6 @@ package com.example.yugioh_forbidden_memories_helper
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.domain.usecases.InitializeDatabaseIfNeededUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.util.Calendar
